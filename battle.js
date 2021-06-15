72a950a6a03cb8b64307bad4493b14d05a61d3f5
agaffff <agafonova.oy@gmail.com>
2021-06-15 18:51:38 +0300

    Добавила контролер

--- battle.js
+++ battle.js
@@ -18,40 +18,78 @@ let model = {
     numShips: 3,
     shipLength: 3,
     shipsSunk: 0,
-    ships=[
-        { location: ["10", "20", "30"], hits: ["", "", ""] },
-        { location: ["42", "43", "44"], hits: ["", "", ""] },
-        { location: ["63", "64", "65"], hits: ["", "", ""] }],
+    ships: [
+        { locations: ["10", "20", "30"], hits: ["", "", ""] },
+        { locations: ["42", "43", "44"], hits: ["", "", ""] },
+        { locations: ["63", "64", "65"], hits: ["", "", ""] }],
     fire: function (guess) {
         for (let i = 0; i < this.numShips; i++) {
             let ship = this.ships[i];
-            location=ship.location;
-            let index=locations.indexOf(guess);
-            if(index>=0){
-                ship.hits[index]="hit";
+
+            let index = ship.locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = "hit";
                 view.displayHit(guess);
                 view.displayMessage("Hit");
-                if(this.isSunk(ship)){
-                    view.displayMessage("You sank my battleship");
+                if (this.isSunk(ship)) {
+                    view.displayMessage("Вы потопили мой корабль");
                     this.shipsSunk++;
                 }
                 return true;
             }
         }
         view.displayMiss(guess);
-        view.displayMessage("You miss");
+        view.displayMessage("Вы промазали");
         return false;
     },
-    isSunk:function(ship){
-        for(let i=0;i<this.shipLength; i++){
-            if(ship.hits[i]!=="hit"){
+    isSunk: function (ship) {
+        for (let i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== "hit") {
                 return false;
             }
         }
         return true;
     }
 
+
 };
 
 
+
+let controller = {
+    guesses: 0,
+    processGuess: function (guess) {
+        let location = parseGuess(guess);
+        if (location) {
+            this.guesses++;
+            let hit = model.fire(location);
+            if (hit && model.shipsSunk === model.numShips) {
+                view.displayMessage("Ты потопил все мои корабли" + this.guesses + "попытки");
+            }
+        }
+    }
+};
+
+function parseGuess(guess) {
+    let alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+    if (guess === null || guess.length !== 2) {
+        alert("Пожалуйста введите букву и цифру");
+    } else {
+        firstChar = guess.charAt(0);
+        let row = alphabet.indexOf(firstChar);
+        let column = guess.charAt(1);
+        if (isNaN(row) || isNaN(column)) {
+            alert("Такого значения нет на доске");
+
+        } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+            alert("Значения за доской");
+        } else {
+            return row + column;
+        }
+    }
+    return null;
+}
+console.log(parseGuess("T6"));
+model.fire("22");
+model.fire("42");
 
